53e430a5375fb74d5db5a6fbbc0e94e99b5cd9ec
vini-cyberpunk <vinitagrawal089@gmail.com>
2026-06-07 18:04:55 +0530

    added some functionalities on admin dashboard

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -6,4 +6,18 @@ admin = Blueprint('admin', __name__)
 @admin.route('/dashboard', methods=['GET'])
 @login_required
 def dashboard():
-	return render_template("admin/dashboard.html")
+
+	from app.models import User, Login, Trek, Booking, Staff
+	
+	total_bookings = Booking.query.count()
+	total_users = User.query.count()
+	total_staff = Staff.query.count()
+	total_treks = Trek.query.count()
+	
+	return render_template(
+		"admin/dashboard.html",
+		total_bookings=total_bookings,
+		total_users=total_users,
+		total_staff=total_staff,
+		total_treks=total_treks
+	)
