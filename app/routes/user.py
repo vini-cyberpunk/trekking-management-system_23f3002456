e33247e1b7ea37d4a85ae75e7059bafe0cea2ad6
vini-cyberpunk <vinitagrawal089@gmail.com>
2026-06-20 23:48:47 +0530

    working on user dashboard

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,3 +8,31 @@ user = Blueprint('user', __name__)
 def dashboard():
 
 	return render_template('user/dashboard.html')
+	
+	
+@user.route('/treks', methods=["GET"])
+@login_required
+def treks():
+	
+	return render_template('user/treks.html')
+	
+
+@user.route('/bookings', methods=["GET"])
+@login_required
+def bookings():
+	
+	return render_template('user/bookings.html')
+	
+	
+@user.route('/history', methods=["GET"])
+@login_required
+def history():
+	
+	return render_template('user/history.html')
+	
+	
+@user.route('/profile', methods=["GET"])
+@login_required
+def profile():
+	
+	return render_template('user/profile.html')
